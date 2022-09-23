7bfa9c7623dfeb921d797c00505bd7517646f88d
Bernat23 <102661677+Bernat23@users.noreply.github.com>
2022-09-23 19:02:12 +0200

    Add files via upload

---
+++ A1.2-Arrays i funcions/arraysMultidimensionals.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+*La funció rep un número n, amb el qual crea un array quadrat de nxn
+*amb una diagonal de * i el triangle inferior omplert amb aleatoris,
+*la superior amb la suma de columna i fila i retorna la matriu
+*/
+function crearMatriu(int $mida) {
+    $matriu= [[]];
+    for($i = 0; $i < $mida; $i++){
+        for($j = 0; $j < $mida; $j++){
+            if($i == $j) {
+                $matriu[$i][$j] = "*";
+            } else if($i < $j) {
+                $matriu[$i][$j] = $i + $j;
+            } else {
+                $matriu[$i][$j] =rand(10,20);
+            }
+        }
+    }
+    return $matriu;
+}
+
+/*
+*Rep una matriu i la representa amb el format de taula de HTML
+*/
+function mostraMatriu($matriu){
+    echo ("<table>");
+    for($i = 0; $i < count($matriu); $i++){
+        echo "<tr> ";
+        for($j = 0; $j < count($matriu[0]); $j++){
+           echo "<td>".$matriu[$i][$j]."</td>"; 
+        }
+        echo "</tr> ";
+    }
+    echo("</table>");
+}
+
+/*
+*Rep una matriu i torna la matriu trasposada
+*/
+function transposaMatriu($matriu){
+    $matriuTrasposada= [[]];
+    for($i = 0; $i < count($matriu[0]); $i++){
+        for($j = 0; $j < count($matriu); $j++){
+            $matriuTrasposada[$i][$j] = $matriu[$j][$i];
+        }
+    }
+    return $matriuTrasposada;
+} 
+
+$mida = 4;
+$matriu = [[]];
+$trasposa = [[]];
+$matriu = crearMatriu($mida);
+mostraMatriu($matriu);
+mostraMatriu(transposaMatriu($matriu));
+?>
---
+++ A1.2-Arrays i funcions/factorial.php
@@ -0,0 +1,35 @@
+<?php
+/*
+*Funció que rep un número positiu o zero i s'encarrega
+*de calcular el seu factorial
+* Retorna un enter
+*/
+function factorial($numero):int {
+    if($numero < 2){
+        return 1;
+    } else {
+        return $numero * factorial($numero - 1);
+    }
+}
+
+/*
+*Funció que rep un array, si l'array està omplert amb 
+* o zero crida la funció factorial, passant-li un número,
+* si és així retorna un array amb tots els resultats
+*Si l'array no està omplert amb només números positius o zero retorna false 
+*/
+function factorialArray(array $numeros) :bool|array {
+    foreach($numeros as $numero) {
+        if(is_int($numero) == false || $numero < 0){
+            return false;
+        } 
+        else if($numero >= 0){
+        $resultat[] = factorial($numero); 
+        }
+    }
+    return $resultat;
+}
+
+$numeros = array(6, 5,10, 0,9,5,4,6);
+print_r(factorialArray($numeros));
+?>
